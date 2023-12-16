29b31d7c4aaffd99df6ec76c094f74b65ed57215
StasMaximov <153751413+StasMaximov@users.noreply.github.com>
2023-12-16 12:14:18 +0300

    task4.cpp

--- task4/task4.cpp
+++ task4/task4.cpp
@@ -8,34 +8,35 @@ using namespace std;
 
 int minMoves(vector<int>& nums) 
 {
-	setlocale(LC_ALL,"Russian");
     int n = nums.size();
     int sum = accumulate(nums.begin(), nums.end(), 0);
     int avg = sum / n;	
-	int moves = 0;
-	for (int i = 0; i < n; i++)
- 	{
+    int moves = 0;
+    for (int i = 0; i < n; i++)
+    {
     moves += abs(nums[i] - avg);
-	}
-	return moves;
+    }
+    return moves;
 }
 
 int main() 
 {
+    setlocale(LC_ALL,"Russian");
     ifstream f("12.txt");
-    if (!f.is_open()) {
-        cout << "Îøèáêà!" << endl;
+    if (!f.is_open()) 
+    {
+        cout << "Ошибка!" << endl;
         return 1;
     }
-    std::vector<int> nums;
+    vector<int> nums;
     int num;
     while (f >> num) 
-	{
+    {
         nums.push_back(num);
     }
     f.close();
     int minMovesCount = minMoves(nums);
-	cout << minMovesCount <<endl;
-	system("pause");
+    cout << minMovesCount <<endl;
+    system("pause");
     return 0;
 }
