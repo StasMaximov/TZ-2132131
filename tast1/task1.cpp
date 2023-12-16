837afaffa5981849dd508fb5d1abcc8cc3beadf6
StasMaximov <153751413+StasMaximov@users.noreply.github.com>
2023-12-16 12:19:51 +0300

    task1.cpp

--- tast1/task1.cpp
+++ tast1/task1.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 int main() 
 {
-	setlocale(LC_ALL,"Russian");
-	int n,m,k=0;
+    int n,m,k=0;
     cin >>n >> m;
     vector<int> arr(n);
     for (int i = 0; i < n; i++) 
-	{
+    {
         arr[i] = i + 1;
     }
     do 
-	{
+    {
         cout << arr[k];
         k = (k + m - 1) % n;
     }while (k != 0);
     cout <<endl;
-	system("pause");
+    system("pause");
     return 0;
 }
 
